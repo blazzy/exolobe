b7dd0594380fa75c1724c15261c45cb36895464a
Krishna Rajendran <krishna@emptybox.org>
2011-07-26 08:59:16 -0400

    c++ gtk restructuring

--- src/gtk_main.cpp
+++ src/gtk_main.cpp
@@ -1,7 +1,28 @@
 extern "C" {
 #include <gtk/gtk.h>
+#include <gdk/gdkkeysyms.h>
 #include "mcdb.h"
 }
+
+struct NameList;
+
+
+struct Window {
+  mcdb_Connection *conn;
+
+  GtkWidget *window;
+  GtkWidget *textView;
+  GtkWidget *searchEntry;
+  NameList  *nameList;
+
+  Window();
+  ~Window();
+  static void destroy();
+  static void searchEntryChanged( GtkWidget *widget, Window *window );
+  static int keySnooper( GtkWidget *widget, GdkEventKey *event, gpointer data );
+  void layout();
+};
+
 
 struct NameList {
   GtkWidget         *treeView;
@@ -18,6 +39,38 @@ struct NameList {
   NameList( GtkWidget *textView );
   static void changed( GtkTreeSelection *widget, GtkTextView *textView );
 };
+
+
+Window::Window() {
+  mcdb_init( &conn );
+
+  window       = gtk_window_new( GTK_WINDOW_TOPLEVEL );
+  searchEntry  = gtk_entry_new();
+  textView     = gtk_text_view_new ();
+  nameList     = new NameList( textView );
+
+  gtk_text_view_set_wrap_mode( GTK_TEXT_VIEW( textView), GTK_WRAP_WORD_CHAR );
+
+
+  layout();
+
+  g_signal_connect( searchEntry, "changed", G_CALLBACK( searchEntryChanged ), this );
+
+  g_signal_connect( window, "destroy", G_CALLBACK( destroy ), NULL );
+  gtk_window_set_title( GTK_WINDOW( window ), "Meta Cortex" );
+
+  gtk_widget_show_all( window );
+
+  searchEntryChanged( searchEntry, this );
+
+  gtk_key_snooper_install( keySnooper, window );
+  gtk_window_set_focus( GTK_WINDOW( window ), 0 );
+}
+
+
+Window::~Window() {
+  mcdb_shutdown( conn );
+}
 
 
 NameList::NameList( GtkWidget *textView ) {
@@ -51,27 +104,26 @@ void NameList::changed( GtkTreeSelection *selection, GtkTextView *textView ) {
 }
 
 
-mcdb_Connection *conn;
-
-
-static void destroy( GtkWidget *widget ) {
+void Window::destroy( ) {
   gtk_main_quit ();
 }
 
 
-static void searchEntryChanged( GtkWidget *widget, GtkListStore *keyStore ) {
+void Window::searchEntryChanged( GtkWidget *widget, Window *window ) {
   const char *text;
   GtkTreeIter iter;
+  GtkListStore *keyStore =  window->nameList->listStore;
+
 
   gtk_list_store_clear( keyStore );
   text = gtk_entry_get_text( GTK_ENTRY( widget ) );
 
   mcdb_Result *result;
 
-  mcdb_find( conn, text, &result );
+  mcdb_find( window->conn, text, &result );
 
   if( result ) {
-    while( !mcdb_next( conn, result ) ) {
+    while( !mcdb_next( window->conn, result ) ) {
       fprintf( stderr, "%s : %s\n", result->a, result->b );
       gtk_list_store_append( keyStore, &iter );
       gtk_list_store_set( keyStore, &iter,
@@ -87,7 +139,7 @@ static void searchEntryChanged( GtkWidget *widget, GtkListStore *keyStore ) {
 }
 
 
-static void layoutWindow( GtkWidget *window, GtkWidget *searchEntry, GtkWidget *nameList, GtkWidget *textView ) {
+void Window::layout() {
   GtkWidget *table            = gtk_table_new( 3, 3, FALSE );
   GtkWidget *textViewScroller = gtk_scrolled_window_new( NULL, NULL );
   GtkWidget *nameListScroller = gtk_scrolled_window_new( NULL, NULL );
@@ -99,7 +151,7 @@ static void layoutWindow( GtkWidget *window, GtkWidget *searchEntry, GtkWidget *
   gtk_table_attach_defaults( GTK_TABLE( table ), textViewScroller , 1 , 3, 1, 3 );
 
   gtk_container_add( GTK_CONTAINER( textViewScroller ), textView );
-  gtk_container_add( GTK_CONTAINER( nameListScroller ), nameList );
+  gtk_container_add( GTK_CONTAINER( nameListScroller ), nameList->treeView );
 
   gtk_table_set_col_spacings( GTK_TABLE( table ), 5 );
 
@@ -107,42 +159,28 @@ static void layoutWindow( GtkWidget *window, GtkWidget *searchEntry, GtkWidget *
 }
 
 
-static GtkWidget *textViewNew( ) {
-  GtkWidget     *textView   = gtk_text_view_new ();
-  gtk_text_view_set_wrap_mode( GTK_TEXT_VIEW( textView), GTK_WRAP_WORD_CHAR );
 
-  return textView;
+int Window::keySnooper( GtkWidget *widget, GdkEventKey *event, gpointer data ) {
+  if( event->keyval == GDK_KEY_q && event->state & GDK_CONTROL_MASK ) {
+    fprintf( stderr, "Quit Key" );
+    destroy( );
+  }
+
+  if( event->keyval == GDK_KEY_Escape ) {
+    gtk_window_set_focus( GTK_WINDOW( data ), 0 );
+  }
+
+  return 0;
 }
 
 
 int main( int argc, char **argv ) {
-  GtkWidget         *window;
-  GtkWidget         *textView;
-  GtkWidget         *searchEntry;
 
   gtk_init( &argc, &argv );
 
-  mcdb_init( &conn );
+  Window window;
 
-  window       = gtk_window_new( GTK_WINDOW_TOPLEVEL );
-  searchEntry  = gtk_entry_new();
-  textView     = textViewNew();
-
-  NameList *nameList = new NameList( textView );
-
-  layoutWindow( window, searchEntry, nameList->treeView, textView );
-
-  g_signal_connect( searchEntry, "changed", G_CALLBACK( searchEntryChanged ), nameList->listStore );
-
-  g_signal_connect( window, "destroy", G_CALLBACK( destroy ), NULL );
-  gtk_window_set_title( GTK_WINDOW( window ), "Meta Cortex" );
-
-  gtk_widget_show_all( window );
-
-  searchEntryChanged( searchEntry, nameList->listStore );
   gtk_main();
-
-  mcdb_shutdown( conn );
 
   return 0;
 }
